4b471f71c6d17ad2cb206d4dec11a3633e4f1708
dima <timon2@ukr.net>
2020-09-17 15:41:15 +0300

    resource.db - init_DB fix

--- src/main/resources/db/init_DB.sql
+++ src/main/resources/db/init_DB.sql
@@ -17,6 +17,8 @@ CREATE TABLE users
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email); --email каждого пользователя должен быть уникальным
 CREATE UNIQUE INDEX users_unique_phone_idx ON users (phone); --phone каждого пользователя должен быть уникальным
+COMMENT ON TABLE users
+    IS 'Пользователи';
 COMMENT ON COLUMN users.id
     IS 'ID пользователя';
 COMMENT ON COLUMN users.name
@@ -31,3 +33,37 @@ COMMENT ON COLUMN users.registered
     IS 'Дата регистраиции пользователя';
 COMMENT ON COLUMN users.enabled
     IS 'true - активный, false - не активный';
+
+---------------role_type---------------2
+CREATE TABLE role_type
+(
+    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    name             VARCHAR                 NOT NULL
+);
+COMMENT ON TABLE role_type
+    IS 'Тип ролей';
+COMMENT ON COLUMN role_type.id
+    IS 'ID';
+COMMENT ON COLUMN role_type.name
+    IS 'Найменование(документооборот, веб...)';
+
+---------------roles---------------3
+CREATE TABLE roles
+(
+    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    name             VARCHAR                 NOT NULL,
+    description      VARCHAR                 NOT NULL,
+    id_role_type     INTEGER                 NOT NULL
+);
+CONSTRAINT roles_role_type_idx UNIQUE (name, id_role_type);
+FOREIGN KEY (id_role_type) REFERENCES role_type (id);
+COMMENT ON TABLE roles
+    IS 'Роли';
+COMMENT ON COLUMN roles.id
+    IS 'ID';
+COMMENT ON COLUMN roles.name
+    IS 'Найменование';
+COMMENT ON COLUMN roles.description
+    IS 'Описание';
+COMMENT ON COLUMN roles.id_role_type
+    IS 'Тип роли';
