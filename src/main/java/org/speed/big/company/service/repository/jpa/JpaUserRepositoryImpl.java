989a2bae27ae22aa5a6bbd4c7cd27674a55c6641
dima <timon2@ukr.net>
2020-09-21 20:53:59 +0300

    org.speed.big.company.service.repository.jpa - JpaUserRepositoryImpl fix

--- src/main/java/org/speed/big/company/service/repository/jpa/JpaUserRepositoryImpl.java
+++ src/main/java/org/speed/big/company/service/repository/jpa/JpaUserRepositoryImpl.java
@@ -4,13 +4,16 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.speed.big.company.service.model.User;
 import org.speed.big.company.service.repository.UserRepository;
+import org.speed.big.company.service.repository.jdbc_impl.row_mapper.UserRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -67,7 +70,11 @@ public class JpaUserRepositoryImpl implements UserRepository {
 
     @Override
     public List<User> getBetweenRegistered(LocalDate startDate, LocalDate endDate) {
-        return null;
+        List<User> list = em.createNamedQuery(User.BETWEEN_REGISTERED)
+                .setParameter("startDate",startDate)
+                .setParameter("endDate",endDate)
+                .getResultList();
+        return list;
     }
 
     @Override
@@ -77,6 +84,79 @@ public class JpaUserRepositoryImpl implements UserRepository {
 
     @Override
     public List<User> filterUser(User user, String sqlCondition) {
-        return null;
+        String sqlFilterUser = "SELECT u FROM User u";
+        int parameterCount = 0;
+
+        if (user.getId() != null) {
+            sqlFilterUser += " where u.id=:id";
+            parameterCount++;
+        }
+        if (user.getName() != null){
+            if (parameterCount > 0)
+                sqlFilterUser += " and u.name=:name";
+            else
+                sqlFilterUser += " where u.name=:name";
+            parameterCount++;
+        }
+        if (user.getEmail() != null){
+            if (parameterCount > 0)
+                sqlFilterUser += " and u.email=:email";
+            else
+                sqlFilterUser += " where u.email=:email";
+            parameterCount++;
+        }
+        if (user.getPassword() != null){
+            if (parameterCount > 0)
+                sqlFilterUser += " and u.password=:password";
+            else
+                sqlFilterUser += " where u.password=:password";
+            parameterCount++;
+        }
+        if (user.getPhone() != null){
+            if (parameterCount > 0)
+                sqlFilterUser += " and u.phone=:phone";
+            else
+                sqlFilterUser += " where u.phone=:phone";
+            parameterCount++;
+        }
+        if (user.getRegistered() != null){
+            if (parameterCount > 0)
+                sqlFilterUser += " and u.registered=:registered";
+            else
+                sqlFilterUser += " where u.registered=:registered";
+            parameterCount++;
+        }
+        if (parameterCount > 0)
+            sqlFilterUser += " and u.enabled=:enabled";
+        else
+            sqlFilterUser += " where u.enabled=:enabled";
+
+        if((sqlCondition != null) && (!sqlCondition.equals(""))){
+            if (parameterCount > 0)
+                sqlFilterUser += " and ( "+sqlCondition+" ) ";
+            else
+                sqlFilterUser = sqlFilterUser + " where ("+sqlCondition+" ) ";
+        }
+
+        Query query = em.createQuery(sqlFilterUser);
+        List<User> list;
+
+        if (user.getId() != null)
+            query.setParameter("id", user.getId());
+        if (user.getName() != null)
+            query.setParameter("name",user.getName());
+        if (user.getEmail() != null)
+            query.setParameter("email",user.getEmail());
+        if (user.getPassword() != null)
+            query.setParameter("password",user.getPassword());
+        if (user.getPhone() != null)
+            query.setParameter("phone",user.getPhone());
+        if (user.getRegistered() != null)
+            query.setParameter("registered",user.getRegistered());
+        query.setParameter("enabled",user.isEnabled());
+
+        list = query.getResultList();
+
+        return list;
     }
 }
