66f21136db223e3e7cf3a8df74c50d47372bf283
dima <timon2@ukr.net>
2020-09-22 20:33:30 +0300

    org.speed.big.company.service.model - RoleType fix

--- src/main/java/org/speed/big/company/service/model/RoleType.java
+++ src/main/java/org/speed/big/company/service/model/RoleType.java
@@ -1,6 +1,26 @@
 package org.speed.big.company.service.model;
 
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@NamedQueries({
+        @NamedQuery(name = RoleType.DELETE, query = "DELETE FROM RoleType rt WHERE rt.id=:id"),
+        @NamedQuery(name = RoleType.ALL_SORTED, query = "SELECT rt FROM RoleType rt order by rt.name")
+        }
+)
+@Entity
+@Table(name = "role_type")
 public class RoleType extends AbstractBaseEntity{
+
+    public static final String DELETE = "RoleType.delete";
+    public static final String ALL_SORTED = "RoleType.allSorted";
+
+    @NotBlank
+    @Size(min = 4)
     private String name;
 
     public RoleType() {
