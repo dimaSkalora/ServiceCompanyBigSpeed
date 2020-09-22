04e61567dfdf7fe6ec5b9275db76aa09959e5b00
dima <timon2@ukr.net>
2020-09-22 16:08:03 +0300

    org.speed.big.company.service.web.role - JspRoleController fix

--- src/main/java/org/speed/big/company/service/web/role/JspRoleController.java
+++ src/main/java/org/speed/big/company/service/web/role/JspRoleController.java
@@ -1,8 +1,12 @@
 package org.speed.big.company.service.web.role;
 
+import org.speed.big.company.service.model.Role;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("roles")
@@ -12,5 +16,26 @@ public class JspRoleController extends AbstractRoleController{
         model.addAttribute("roles", super.getAll());
         return "roles/roles";
     }
+
+    @RequestMapping(value = "/role", method = RequestMethod.GET)
+    public ModelAndView role(){
+        ModelAndView modelAndView = new ModelAndView("roles/role");
+        modelAndView.addObject("role", new Role());
+        modelAndView.addObject("allRoleType", super.getAllRoleType());
+
+        return modelAndView;
+    }
+
+    @GetMapping("/filter")
+    public String filter(){
+        return "roles/roleFilter";
+    }
+
+    @RequestMapping(value = "/createRequestParam", method = RequestMethod.POST)
+    public String createRequestParam(){
+
+        return "redirect:/roles";
+    }
+
 
 }
