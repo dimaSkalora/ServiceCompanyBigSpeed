b588f0fe552f392cfbbf17eed1b63d2137d45351
dima <timon2@ukr.net>
2020-09-22 14:57:49 +0300

    org.speed.big.company.service.service.* - add RoleService, RoleServiceImpl

---
+++ src/main/java/org/speed/big/company/service/service/RoleService.java
@@ -0,0 +1,15 @@
+package org.speed.big.company.service.service;
+
+import org.speed.big.company.service.model.Role;
+import org.speed.big.company.service.util.exception.NotFoundException;
+
+import java.util.List;
+
+public interface RoleService {
+    Role create(Role role);
+    void update(Role role) throws NotFoundException;//NotFoundException - Об'экт не обнаружен
+    Role get(int id) throws NotFoundException;
+    boolean delete(int id) throws NotFoundException;
+    List<Role> getAll();
+    List<Role> filter(Role role);
+}
---
+++ src/main/java/org/speed/big/company/service/service/impl/RoleServiceImpl.java
@@ -0,0 +1,59 @@
+package org.speed.big.company.service.service.impl;
+
+import org.speed.big.company.service.model.Role;
+import org.speed.big.company.service.repository.RoleRepository;
+import org.speed.big.company.service.service.RoleService;
+import org.speed.big.company.service.util.exception.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+import static org.speed.big.company.service.util.ValidationUtil.checkNotFoundWithId;
+
+@Service("roleServiceImpl")
+public class RoleServiceImpl implements RoleService {
+   /* @Autowired
+    @Qualifier("jdbcRoleRepositoryImpl")*/
+    private final RoleRepository roleRepository;
+
+    public RoleServiceImpl(@Qualifier("jdbcRoleRepositoryImpl") RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public Role create(Role role) {
+        Assert.notNull(role,"не должно быть null");
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public void update(Role role) throws NotFoundException {
+        Assert.notNull(role,"не должно быть null");
+        roleRepository.save(role);
+    }
+
+    @Override
+    public Role get(int id) throws NotFoundException {
+        //Проверка - не найден с идентификатором
+        return checkNotFoundWithId(roleRepository.get(id),id);
+    }
+
+    @Override
+    public boolean delete(int id) throws NotFoundException {
+        return checkNotFoundWithId(Boolean.valueOf(roleRepository.delete(id)),id);
+    }
+
+    @Override
+    public List<Role> getAll() {
+        return roleRepository.getAll();
+    }
+
+    @Override
+    public List<Role> filter(Role role) {
+        Assert.notNull(role,"не должно быть null");
+        return roleRepository.filter(role);
+    }
+}
