31a5719d767069ec99c6853b1e14a61edf77197b
dima <timon2@ukr.net>
2020-09-23 14:53:39 +0300

    org.speed.big.company.service.web.user - add AdminRestUserController

---
+++ src/main/java/org/speed/big/company/service/web/user/AdminRestUserController.java
@@ -0,0 +1,74 @@
+package org.speed.big.company.service.web.user;
+
+import org.speed.big.company.service.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController()
+@RequestMapping(org.speed.big.company.service.web.user.AdminRestUserController.REST_URL)
+public class AdminRestUserController extends AbstractUserController{
+
+    static final String REST_URL = "/rest/admin/users";
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    //produces - Воспроизводимые типы носителей запрошенного запроса, сужающие первичное отображение.
+    public ResponseEntity<List<User>> getAllU() {
+        List<User> list = super.getAll();
+
+        return list != null &&  !list.isEmpty()
+                ? new ResponseEntity<>(list, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    //@PathVariable - Аннотации, указывающие, что параметр метода должен быть привязан к переменной шаблона UR
+    public ResponseEntity<User> getU(@PathVariable("id") int id) {
+        User user = super.get(id);
+
+        return user != null
+                ? new ResponseEntity<>(user, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    //consumes - Типы расходных материалов отображаемого запроса, сужающие первичное отображение.
+    //RequestBody - Аннотации, указывающие параметр метода, должны быть привязаны к телу веб-запроса.
+    // Тело запроса передается через HttpMessageConverter для разрешения аргумента метода в зависимости
+    // от типа содержимого запроса. Необязательно, автоматическая проверка может быть применена
+    // путем аннотации аргумента с помощью @Valid.
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> createWithLocation(@RequestBody User user) {
+        User created = super.create(user);
+
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        httpHeaders.setLocation(uriOfNewResource);
+
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+
+        //created - Создайте новый построитель с статусом CREATED и заголовком местоположения, заданным для данного URI.
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+
+    @GetMapping(value = "/text")
+    public String testUTF() {
+        return "Русский текст";
+    }
+
+    @GetMapping(value = "/text2",produces = MediaType.APPLICATION_JSON_VALUE)
+    public String testUTF2() {
+        return "Русский текст";
+    }
+}
