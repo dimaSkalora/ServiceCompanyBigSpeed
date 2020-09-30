bac437ee9b568c9a05d202562cd866707980f8a2
dima <timon2@ukr.net>
2020-09-30 21:23:21 +0300

    org.speed.big.company.service.web.role - RestRoleController fix

--- src/main/java/org/speed/big/company/service/web/role/RestRoleController.java
+++ src/main/java/org/speed/big/company/service/web/role/RestRoleController.java
@@ -1,11 +1,96 @@
 package org.speed.big.company.service.web.role;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.speed.big.company.service.model.Role;
+import org.speed.big.company.service.model.RoleType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(RestRoleController.REST_URL)
 public class RestRoleController extends AbstractRoleController{
     static final String REST_URL = "rest/roles";
+
+    //produces - Какой формат отправляем клиенту
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Role> getAll(){
+        List<Role> list = super.getAll();
+        return list;
+    }
+
+    //@PathVariable - Аннотации, указывающие, что параметр метода должен быть привязан к переменной шаблона UR
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Role> getRole(@PathVariable int id){
+        Role role = super.get(id);
+
+        return role != null
+                ? new ResponseEntity<>(role, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    //consumes - Какой формат получаем(от клиента)
+    //produces - Какой формат отправляем клиенту
+    //RequestBody - Аннотации, указывающие параметр метода, должны быть привязаны к телу веб-запроса.
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Role> createWithLocation(@RequestBody Role role){
+        Role created = super.create(role);
+
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL+"/{id}")
+                .buildAndExpand(created.getId()).toUri();
+
+        //HttpHeaders httpHeaders = new HttpHeaders();
+        //httpHeaders.setLocation(uriOfNewResource);
+        //created - Создайте новый построитель с статусом CREATED и заголовком местоположения, заданным для данного URI.
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Role> updateRole(@RequestBody Role role){
+        Role updateRole = super.update(role);
+
+        return updateRole != null
+                ? new ResponseEntity<>(updateRole, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteRole(@PathVariable int id){
+        super.delete(id);
+    }
+
+    @PostMapping(value = "/filter",
+            consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Role>> filter(@RequestBody Role role){
+        List<Role> filterRoles = super.filterRole(role);
+
+        return filterRoles != null
+                ? new ResponseEntity<>(filterRoles, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping()
+    public List<RoleType> getAllRoleType(){
+        List<RoleType> list = super.getAllRoleType();
+
+        return list;
+    }
+
+    @GetMapping(value = "/getRoleType/{id}")
+    public RoleType getRoleType(@PathVariable int id){
+        return super.getRoleType(id);
+    }
+
+
+
+
+
+
 
 }
