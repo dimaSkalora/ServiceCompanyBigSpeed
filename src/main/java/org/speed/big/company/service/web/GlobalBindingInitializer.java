882d4d7ac84605dbc95adb8a2a1ef41733f3be51
dima <timon2@ukr.net>
2020-10-18 18:49:41 +0300

    org.speed.big.company.service.web - GlobalBindingInitializer fix

--- src/main/java/org/speed/big/company/service/web/GlobalBindingInitializer.java
+++ src/main/java/org/speed/big/company/service/web/GlobalBindingInitializer.java
@@ -7,9 +7,11 @@ import org.speed.big.company.service.model.propertyeditor.RolePropertyEditor;
 import org.speed.big.company.service.model.propertyeditor.RoleTypePropertyEditor;
 import org.speed.big.company.service.model.propertyeditor.UserPropertyEditor;
 import org.speed.big.company.service.model.propertyeditor.workflow.WFBaseProcessTypePropertyEditor;
+import org.speed.big.company.service.model.propertyeditor.workflow.WFPackagePropertyEditor;
 import org.speed.big.company.service.model.propertyeditor.workflow.WFPackageStatusPropertyEditor;
 import org.speed.big.company.service.model.propertyeditor.workflow.WFServicePropertyEditor;
 import org.speed.big.company.service.model.workflow.WFBaseProcessType;
+import org.speed.big.company.service.model.workflow.WFPackage;
 import org.speed.big.company.service.model.workflow.WFPackageStatus;
 import org.speed.big.company.service.model.workflow.WFService;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
@@ -73,5 +75,6 @@ public class GlobalBindingInitializer {
         binder.registerCustomEditor(WFPackageStatus.class, new WFPackageStatusPropertyEditor());
         binder.registerCustomEditor(WFService.class, new WFServicePropertyEditor());
         binder.registerCustomEditor(WFBaseProcessType.class, new WFBaseProcessTypePropertyEditor());
+        binder.registerCustomEditor(WFPackage.class, new WFPackagePropertyEditor());
     }
 }
