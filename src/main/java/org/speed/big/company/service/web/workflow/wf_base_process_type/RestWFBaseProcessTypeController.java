b8e52bbc33bd3bab46dc99f30e8c24c86d9891fb
dima <timon2@ukr.net>
2020-11-11 15:09:45 +0200

    org.speed.big.company.service.web.workflow.wf_base_process_type - add RestWFBaseProcessTypeController

---
+++ src/main/java/org/speed/big/company/service/web/workflow/wf_base_process_type/RestWFBaseProcessTypeController.java
@@ -0,0 +1,80 @@
+package org.speed.big.company.service.web.workflow.wf_base_process_type;
+
+import org.speed.big.company.service.model.workflow.WFBaseProcessType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(RestWFBaseProcessTypeController.REST_URL)
+public class RestWFBaseProcessTypeController extends AbstractWFBaseProcessTypeController{
+    final static String REST_URL = "/rest/workflow/wfBaseProcessTypes";
+
+    //produces - Какой формат отправляем клиенту
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<WFBaseProcessType> wfBaseProcessTypes(){
+        return super.getAll();
+    }
+
+    @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<WFBaseProcessType> getWFBPT(@PathVariable int id){
+        WFBaseProcessType wfBaseProcessType = super.get(id);
+
+        return wfBaseProcessType != null
+                ? new ResponseEntity<>(wfBaseProcessType, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    //consumes - Какой формат получаем(от клиента)
+    //produces - Какой формат отправляем клиенту
+    //RequestBody - Аннотации, указывающие параметр метода, должны быть привязаны к телу веб-запроса.
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<WFBaseProcessType> createWithLocation(@RequestBody WFBaseProcessType wfBaseProcessType){
+        WFBaseProcessType created = super.create(wfBaseProcessType);
+
+        URI uriOfResources = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL+"{id}")
+                .buildAndExpand(wfBaseProcessType.getId()).toUri();
+
+        //HttpHeaders httpHeaders = new HttpHeaders();
+        //httpHeaders.setLocation(uriOfNewResource);
+        //created - Создайте новый построитель с статусом CREATED и заголовком местоположения, заданным для данного URI.
+        return ResponseEntity.created(uriOfResources).body(created);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<WFBaseProcessType> updateWFBPT(@RequestBody WFBaseProcessType wfBaseProcessType){
+        WFBaseProcessType updated = super.update(wfBaseProcessType);
+
+        return updated != null
+                ? new ResponseEntity<>(updated, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void deleteWFBPT(int id){
+        super.delete(id);
+    }
+
+    //consumes - Какой формат получаем(от клиента)
+    //produces - Какой формат отправляем клиенту
+    //RequestBody - Аннотации, указывающие параметр метода, должны быть привязаны к телу веб-запроса.
+    @PostMapping(value = "/filter", consumes = MediaType.APPLICATION_JSON_VALUE
+                ,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<WFBaseProcessType>> filterWFBPT(@RequestBody WFBaseProcessType wfBaseProcessType){
+        List<WFBaseProcessType> list = super.filter(wfBaseProcessType);
+
+        return list != null
+                ? new ResponseEntity<>(list,HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+
+}
