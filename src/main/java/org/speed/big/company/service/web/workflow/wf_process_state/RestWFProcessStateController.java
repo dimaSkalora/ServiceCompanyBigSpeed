722cec2113fb6e9ec7af6d47b73243f71a2f0d81
dima <timon2@ukr.net>
2020-11-23 14:58:21 +0200

    org.speed.big.company.service.web.workflow.wf_process_state - RestWFProcessStateController

---
+++ src/main/java/org/speed/big/company/service/web/workflow/wf_process_state/RestWFProcessStateController.java
@@ -0,0 +1,82 @@
+package org.speed.big.company.service.web.workflow.wf_process_state;
+
+import org.speed.big.company.service.model.workflow.WFProcessState;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(RestWFProcessStateController.REST_URL)
+public class RestWFProcessStateController extends AbstractWFProcessStateController{
+    final static String REST_URL = "/rest/workflow/wfProcessStates";
+
+    //produces - Какой формат отправляем клиенту
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<WFProcessState>> wfProcessStates(){
+        List<WFProcessState> list = super.getAll();
+
+        return list != null
+                ? new ResponseEntity<>(list, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<WFProcessState> wfProcessState(@PathVariable int id){
+        WFProcessState wfProcessState = super.get(id);
+
+        return wfProcessState != null
+                ? new ResponseEntity<>(wfProcessState,HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    //consumes - Какой формат получаем(от клиента)
+    //produces - Какой формат отправляем клиенту
+    //RequestBody - Аннотации, указывающие параметр метода, должны быть привязаны к телу веб-запроса.
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<WFProcessState> createWithLocation(@RequestBody WFProcessState wfProcessState){
+        WFProcessState created = super.create(wfProcessState);
+
+        URI uriOfResources = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL+"{id}")
+                .buildAndExpand(wfProcessState.getId()).toUri();
+
+        //HttpHeaders httpHeaders = new HttpHeaders();
+        //httpHeaders.setLocation(uriOfNewResource);
+
+        //created - Создайте новый построитель с статусом CREATED и заголовком местоположения, заданным для данного URI.
+        return ResponseEntity.created(uriOfResources).body(created);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<WFProcessState> updateWFPS(@RequestBody WFProcessState wfProcessState){
+        WFProcessState updated = super.update(wfProcessState);
+
+        return updated != null
+                ? new ResponseEntity<>(updated,HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void deleteWFPS(@PathVariable int id){
+        super.delete(id);
+    }
+
+    //consumes - Какой формат получаем(от клиента)
+    //produces - Какой формат отправляем клиенту
+    //RequestBody - Аннотации, указывающие параметр метода, должны быть привязаны к телу веб-запроса.
+    @PostMapping(value = "/filter",consumes = MediaType.APPLICATION_JSON_VALUE,
+                produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<WFProcessState>> filterWFPS(@RequestBody WFProcessState wfProcessState){
+        List<WFProcessState> list = super.filter(wfProcessState);
+
+        return list != null
+                ? new ResponseEntity<>(list,HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
