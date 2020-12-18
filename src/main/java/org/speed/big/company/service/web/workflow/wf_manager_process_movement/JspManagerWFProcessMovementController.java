d736e8d4e151941acc66b732353bbca158714856
dima <timon2@ukr.net>
2020-12-18 10:57:29 +0200

    org.speed.big.company.service.web.workflow.wf_manager_process_movement - JspManagerWFProcessMovementController fix (add getWFProcessPackages(Получить процесс пакета по текущему движению) )

--- src/main/java/org/speed/big/company/service/web/workflow/wf_manager_process_movement/JspManagerWFProcessMovementController.java
+++ src/main/java/org/speed/big/company/service/web/workflow/wf_manager_process_movement/JspManagerWFProcessMovementController.java
@@ -1,15 +1,14 @@
 package org.speed.big.company.service.web.workflow.wf_manager_process_movement;
 
 import org.speed.big.company.service.model.Role;
-import org.speed.big.company.service.model.workflow.WFProcess;
-import org.speed.big.company.service.model.workflow.WFProcessMovement;
-import org.speed.big.company.service.model.workflow.WFProcessStatus;
-import org.speed.big.company.service.model.workflow.WFService;
+import org.speed.big.company.service.model.workflow.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -28,9 +27,9 @@ public class JspManagerWFProcessMovementController extends AbstractManagerWFProc
         return "workflow/managerWFProcessMovements/managerWFProcessMovements";
     }
 
-    @GetMapping("/wfProcessMovements/{roleId,}/{wfServiceId}/{indexList}")
-    public String wfProcessMovements(@PathVariable int roleId, @PathVariable int wfServiceId,
-                                            @PathVariable int indexList, Model model){
+    @GetMapping("/wfProcessMovements")
+    public String wfProcessMovements(@RequestParam int roleId, @RequestParam int wfServiceId,
+                                     @RequestParam int indexList, Model model){
         List<WFProcessMovement> wfProcessMovementList = null;
         List<WFProcess> wfProcessList = null;
 
@@ -46,5 +45,24 @@ public class JspManagerWFProcessMovementController extends AbstractManagerWFProc
 
         return "workflow/managerWFProcessMovements/managerWFProcessMovements";
     }
+
+    @GetMapping("/wfProcessPackage/{wfPackId}/{wfProcessId}/{wfBaseProcessId}")
+    public ModelAndView getWFProcessPackages(@PathVariable int wfPackId, @PathVariable int wfProcessId,
+                                             @PathVariable int wfBaseProcessId){
+        ModelAndView modelAndView = new ModelAndView("workflow/managerWFProcessMovements/wfProcessPackage");
+        WFPackage wfPackage = super.getWFPackageOfProcessMovement(wfPackId);
+        WFProcess wfProcess = super.getWFProcessById(wfProcessId);
+        List<WFProcess> wfProcessList = super.getAllWFProcessByWFPackageId(wfPackage.getId());
+        List<WFProcessMovement> wfProcessMovementList =
+                super.getListWFPMByProcessAndBaseProcess(wfProcessId, wfBaseProcessId);
+
+        modelAndView.addObject("wfPackage",wfPackage);
+        modelAndView.addObject("wfProcess",wfProcess);
+        modelAndView.addObject("wfProcessList",wfProcessList);
+        modelAndView.addObject("wfProcessMovementList",wfProcessMovementList);
+
+        return modelAndView;
+    }
+
 
 }
