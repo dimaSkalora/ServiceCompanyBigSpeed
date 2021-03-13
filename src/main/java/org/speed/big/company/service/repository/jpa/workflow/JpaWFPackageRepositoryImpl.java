09b2c37151af7f9b9194c8c57887508e4b4ff0fe
dima <timon2@ukr.net>
2021-03-13 10:25:00 +0200

    org.speed.big.company.service.repository.jpa.workflow - JpaWFPackageRepositoryImpl

--- src/main/java/org/speed/big/company/service/repository/jpa/workflow/JpaWFPackageRepositoryImpl.java
+++ src/main/java/org/speed/big/company/service/repository/jpa/workflow/JpaWFPackageRepositoryImpl.java
@@ -29,12 +29,11 @@ public class JpaWFPackageRepositoryImpl implements WFPackageRepository {
 
     @Override
     public WFPackage get(int id) {
+        /*WFPackage wfPackage = entityManager.find(WFPackage.class, id);*/
 
-/*        WFPackage wfPackage = entityManager.createNamedQuery(WFPackage.GET, WFPackage.class)
+        WFPackage wfPackage = entityManager.createNamedQuery(WFPackage.GET, WFPackage.class)
                 .setParameter("id", id)
-                .getSingleResult();*/
-
-        WFPackage wfPackage = entityManager.find(WFPackage.class, id);
+                .getSingleResult();
 
         return wfPackage;
     }
@@ -42,8 +41,8 @@ public class JpaWFPackageRepositoryImpl implements WFPackageRepository {
     @Transactional
     @Override
     public boolean delete(int id) {
-        WFPackage wfPackage = entityManager.getReference(WFPackage.class, id);
-        entityManager.remove(wfPackage);
+/*        WFPackage wfPackage = entityManager.getReference(WFPackage.class, id);
+        entityManager.remove(wfPackage);*/
 
      /*   boolean queryDelete = entityManager
                 .createQuery("delete from WFPackage wfp where wfp.id=:id")
@@ -72,7 +71,8 @@ public class JpaWFPackageRepositoryImpl implements WFPackageRepository {
 
     @Override
     public List<WFPackage> filter(WFPackage wfPackage, String sqlCondition) {
-        StringBuilder queryFilter = new StringBuilder("select wfp from WFPackage\n");
+        StringBuilder queryFilter = new StringBuilder("select wfp from WFPackage wfp " +
+                " join fetch wfp.wfServiceId join fetch wfp.wfPackageStatusId \n");
         int paramCount = 0;
         List<WFPackage> list;
 
@@ -89,51 +89,51 @@ public class JpaWFPackageRepositoryImpl implements WFPackageRepository {
         }
         if (wfPackage.getDateRegistration() != null) {
             if (paramCount == 0)
-                queryFilter.append(" where wfp.date_registration=:dateRegistration\n");
+                queryFilter.append(" where wfp.dateRegistration=:dateRegistration\n");
             else
-                queryFilter.append("and wfp.date_registration=:dateRegistration\n");
+                queryFilter.append("and wfp.dateRegistration=:dateRegistration\n");
             paramCount++;
         }
         if (wfPackage.getCustomerName() != null) {
             if (paramCount == 0)
-                queryFilter.append(" where wfp.customer_name=:customerName\n");
+                queryFilter.append(" where wfp.customerName=:customerName\n");
             else
-                queryFilter.append("and wfp.customer_name=:customerName\n");
+                queryFilter.append("and wfp.customerName=:customerName\n");
             paramCount++;
         }
         if (wfPackage.getCustomerAddress() != null) {
             if (paramCount == 0)
-                queryFilter.append(" where wfp.customer_address=:customerAddress\n");
+                queryFilter.append(" where wfp.customerAddress=:customerAddress\n");
             else
-                queryFilter.append("and wfp.customer_address=:customerAddress\n");
+                queryFilter.append("and wfp.customerAddress=:customerAddress\n");
             paramCount++;
         }
         if (wfPackage.getCustomerAddressJur() != null) {
             if (paramCount == 0)
-                queryFilter.append(" where wfp.customer_address_jur=:customerAddressJur\n");
+                queryFilter.append(" where wfp.customerAddressJur=:customerAddressJur\n");
             else
-                queryFilter.append("and wfp.customer_address_jur=:customerAddressJur\n");
+                queryFilter.append("and wfp.customerAddressJur=:customerAddressJur\n");
             paramCount++;
         }
         if (wfPackage.getCustomerPhone() != null) {
             if (paramCount == 0)
-                queryFilter.append(" where wfp.customer_phone=:customerPhone\n");
+                queryFilter.append(" where wfp.customerPhone=:customerPhone\n");
             else
-                queryFilter.append("and wfp.customer_phone=:customerPhone\n");
+                queryFilter.append("and wfp.customerPhone=:customerPhone\n");
             paramCount++;
         }
         if (wfPackage.getCustomerEmail() != null) {
             if (paramCount == 0)
-                queryFilter.append(" where wfp.customer_email=:customerEmail\n");
+                queryFilter.append(" where wfp.customerEmail=:customerEmail\n");
             else
-                queryFilter.append("and wfp.customer_email=:customerEmail\n");
+                queryFilter.append("and wfp.customerEmail=:customerEmail\n");
             paramCount++;
         }
         if (wfPackage.getContractNumber() != null) {
             if (paramCount == 0)
-                queryFilter.append(" where wfp.contract_number=:contractNumber\n");
+                queryFilter.append(" where wfp.contractNumber=:contractNumber\n");
             else
-                queryFilter.append("and wfp.contract_number=:contractNumber\n");
+                queryFilter.append("and wfp.contractNumber=:contractNumber\n");
             paramCount++;
         }
         if (wfPackage.getDescription() != null) {
@@ -145,44 +145,44 @@ public class JpaWFPackageRepositoryImpl implements WFPackageRepository {
         }
         if (wfPackage.getUserAdd() != null) {
             if (paramCount == 0)
-                queryFilter.append(" where wfp.user_add=:userAdd\n");
+                queryFilter.append(" where wfp.userAdd=:userAdd\n");
             else
-                queryFilter.append("and wfp.user_add=:userAdd\n");
+                queryFilter.append("and wfp.userAdd=:userAdd\n");
             paramCount++;
         }
         if (wfPackage.getDateAdd() != null) {
             if (paramCount == 0)
-                queryFilter.append(" where wfp.date_add=:dateAdd\n");
+                queryFilter.append(" where wfp.dateAdd=:dateAdd\n");
             else
-                queryFilter.append("and wfp.date_add=:dateAdd\n");
+                queryFilter.append("and wfp.dateAdd=:dateAdd\n");
             paramCount++;
         }
         if (wfPackage.getUserEdit() != null) {
             if (paramCount == 0)
-                queryFilter.append(" where wfp.user_edit=:userEdit\n");
+                queryFilter.append(" where wfp.userEdit=:userEdit\n");
             else
-                queryFilter.append("and wfp.user_edit=:userEdit\n");
+                queryFilter.append("and wfp.userEdit=:userEdit\n");
             paramCount++;
         }
         if (wfPackage.getDateEdit() != null) {
             if (paramCount == 0)
-                queryFilter.append(" where wfp.date_edit=:dateEdit\n");
+                queryFilter.append(" where wfp.dateEdit=:dateEdit\n");
             else
-                queryFilter.append("and wfp.date_edit=:dateEdit\n");
+                queryFilter.append("and wfp.dateEdit=:dateEdit\n");
             paramCount++;
         }
         if (wfPackage.getWfServiceId() != null) {
             if (paramCount == 0)
-                queryFilter.append(" where wfp.wf_service_id=:wfServiceId\n");
+                queryFilter.append(" where wfp.wfServiceId=:wfServiceId\n");
             else
-                queryFilter.append("and wfp.wf_service_id=:wfServiceId\n");
+                queryFilter.append("and wfp.wfServiceId=:wfServiceId\n");
             paramCount++;
         }
         if (wfPackage.getWfPackageStatusId() != null) {
             if (paramCount == 0)
-                queryFilter.append(" where wfp.wf_package_status_id=:wfPackageStatusId\n");
+                queryFilter.append(" where wfp.wfPackageStatusId=:wfPackageStatusId\n");
             else
-                queryFilter.append("and wfp.wf_package_status_id=:wfPackageStatusId\n");
+                queryFilter.append("and wfp.wfPackageStatusId=:wfPackageStatusId\n");
             paramCount++;
         }
 
@@ -193,7 +193,7 @@ public class JpaWFPackageRepositoryImpl implements WFPackageRepository {
                 queryFilter.append(" and ( "+sqlCondition+" )");
         }
 
-        queryFilter.append("order by wfp.dateRegistration, wfp.name "); //default ASC -  по возрастанию
+        queryFilter.append(" order by wfp.dateRegistration, wfp.name "); //default ASC -  по возрастанию
 
         Query query = entityManager.createQuery(queryFilter.toString());
 
