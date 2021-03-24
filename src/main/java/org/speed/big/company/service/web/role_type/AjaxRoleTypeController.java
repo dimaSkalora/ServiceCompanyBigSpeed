f36cf31ed9379c52fa57fb02bf06ff031d3d5753
dima <timon2@ukr.net>
2021-03-24 11:12:33 +0200

    org.speed.big.company.service.web.role_type - add AjaxRoleTypeController

---
+++ src/main/java/org/speed/big/company/service/web/role_type/AjaxRoleTypeController.java
@@ -0,0 +1,46 @@
+package org.speed.big.company.service.web.role_type;
+
+import org.speed.big.company.service.model.RoleType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(org.speed.big.company.service.web.role_type.AjaxRoleTypeController.REST_URL)
+public class AjaxRoleTypeController extends AbstractRoleTypeController {
+    static final String REST_URL = "/ajax/roleTypes";
+
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<RoleType> getAll() {
+        return super.getAll();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public RoleType getRT(@PathVariable int id) {
+        return super.get(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createOrUpdate(RoleType roleType){
+         if (roleType.isNew())
+             super.create(roleType);
+         else
+             super.update(roleType);
+
+         return new ResponseEntity<>( HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteRT(@PathVariable int id){
+        super.delete(id);
+    }
+
+    @PostMapping(value = "/filter", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<RoleType> filter(RoleType roleType){
+        return super.filterRoleType(roleType);
+    }
+}
