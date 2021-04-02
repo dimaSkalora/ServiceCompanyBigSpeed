8539ff9f0108f45ac5a6ed11c5ba8ebe54b96336
dima <timon2@ukr.net>
2021-04-02 09:50:38 +0300

    org.speed.big.company.service.web.workflow.wf_process_state - add AjaxWFProcessStateController

---
+++ src/main/java/org/speed/big/company/service/web/workflow/wf_process_state/AjaxWFProcessStateController.java
@@ -0,0 +1,45 @@
+package org.speed.big.company.service.web.workflow.wf_process_state;
+
+import org.speed.big.company.service.model.workflow.WFProcessState;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(AjaxWFProcessStateController.REST_URL)
+public class AjaxWFProcessStateController  extends AbstractWFProcessStateController{
+    final static String REST_URL = "/ajax/workflow/wfProcessStates";
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<WFProcessState> wfProcessStates(){
+        return super.getAll();
+    }
+
+    @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public WFProcessState wfProcessState(@PathVariable int id){
+        return  super.get(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createOrUpdate(WFProcessState wfProcessState){
+        if (wfProcessState.isNew())
+            super.create(wfProcessState);
+        else
+            super.update(wfProcessState);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteWFPS(@PathVariable int id){
+        super.delete(id);
+    }
+
+    @PostMapping(value = "/filter",produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<WFProcessState> filterWFPS(WFProcessState wfProcessState){
+        return super.filter(wfProcessState);
+    }
+}
