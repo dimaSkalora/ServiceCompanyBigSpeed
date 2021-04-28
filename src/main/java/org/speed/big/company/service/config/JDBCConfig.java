2aedf9f434c506dd3f24830aef12bd269ea8de20
dima <timon2@ukr.net>
2021-04-28 16:56:39 +0300

    org.speed.big.company.service.config - add JDBCConfig

---
+++ src/main/java/org/speed/big/company/service/config/JDBCConfig.java
@@ -0,0 +1,32 @@
+package org.speed.big.company.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/*@Configuration
+@EnableTransactionManagement
+@ComponentScan("org.speed.big.company.service.repository.jdbc_impl")*/
+public class JDBCConfig {
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public PlatformTransactionManager dataSourceTransactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
