2c5635b02ba6e56b688efaad379cecffa590ce71
dima <timon2@ukr.net>
2021-04-30 14:10:01 +0300

    test.org.speed.big.company.service.repository.jdbc_impl - JdbcUserRepositoryImplTest, UserServiceImplTest fix

--- src/test/java/test/org/speed/big/company/service/service/UserServiceImplTest.java
+++ src/test/java/test/org/speed/big/company/service/service/UserServiceImplTest.java
@@ -1,7 +1,7 @@
 package test.org.speed.big.company.service.service;
 
-import org.junit.Before;
-import org.junit.Test;
+//import org.junit.Before;
+//import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.speed.big.company.service.model.User;
