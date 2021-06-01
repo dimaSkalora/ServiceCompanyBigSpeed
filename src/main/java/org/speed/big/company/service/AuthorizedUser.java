421912ec6fc4baea8c0ec86472ee0bda9c0d9632
dima <timon2@ukr.net>
2021-06-01 14:30:14 +0300

    org.speed.big.company.service - add AuthorizedUser

---
+++ src/main/java/org/speed/big/company/service/AuthorizedUser.java
@@ -0,0 +1,28 @@
+package org.speed.big.company.service;
+
+import org.speed.big.company.service.model.AbstractBaseEntity;
+import org.speed.big.company.service.model.User;
+
+public class AuthorizedUser {
+
+    private static User user;
+
+    public static User getUser() {
+        return user;
+    }
+
+    public static void setUser(User user) {
+        AuthorizedUser.user = user;
+    }
+
+    private static int id = AbstractBaseEntity.START_SEQ;
+
+    public static int id() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        AuthorizedUser.id = id;
+    }
+
+}
