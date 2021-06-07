d04c6b1a002159c950218b88b2e1eed0758c4db3
dima <timon2@ukr.net>
2021-06-07 12:05:00 +0300

    org.speed.big.company.service.web - RootController fix

--- src/main/java/org/speed/big/company/service/web/RootController.java
+++ src/main/java/org/speed/big/company/service/web/RootController.java
@@ -1,14 +1,54 @@
 package org.speed.big.company.service.web;
 
+import org.speed.big.company.service.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RootController {
+
+    @GetMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String loginPage(@RequestParam(value = "error", required = false) String error,
+                            @RequestParam(value = "logout", required = false) String logout,
+                            Model model) {
+        String errorMessge = null;
+        if(error != null) {
+            errorMessge = "Username or Password is incorrect !!";
+        }
+        if(logout != null) {
+            errorMessge = "You have been successfully logged out !!";
+        }
+        model.addAttribute("errorMessge", errorMessge);
+        return "login";
+    }
+
     @RequestMapping("/")
-    public String index(){
-        return "index";
+    public String root(){
+        return "home";
+    }
+
+    @RequestMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile";
+    }
+
+    @GetMapping("/register")
+    public String register(ModelMap model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("register", true);
+        return "profile";
     }
 
     @RequestMapping("/restView")
